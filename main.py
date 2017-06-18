e93be7acfffc32495b53bd2ee5f0497b295958b8
Kevin Tam <tam.kevin@hotmail.com>
2017-06-18 14:34:24 -0700

    update style sheet to properly display the text area

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ form = """
                 font: 16px sans-serif;
                 border-radius: 10px;
             }
-            textarea {
+            .textarea {
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
             }
-            h1{
+            header h1{
                 text-align: center;
             }
             body{
@@ -36,13 +36,15 @@ form = """
     </head>
     <body>
       <!-- create your form here -->
-      <h1>Web Caesar</h1>
+        <header>
+            <h1>Web Caesar</h1>
+        </header>
       <form method='post'>
         <label>
             Rotate by:
             <input type='text' name='rot' / >
         </label>
-        <input type='textarea' name='text' />
+        <input class='textarea' type='textarea' name='text' />
         <input type='submit' value='Encrypt Message!' />
 
       </form>
